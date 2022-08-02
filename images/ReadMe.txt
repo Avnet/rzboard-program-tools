74d33b3f46166bef045f01a4badbc1e7d4757d16
wenxue.guo <wenxue.guo@avnet.com>
2022-08-02 09:44:30 +0800

    Add RzBoard flash program tools
    Signed-off-by: wenxue.guo <wenxue.guo@avnet.com>

---
+++ images/ReadMe.txt
@@ -0,0 +1,1 @@
+Please put bootloader and system image files here!
---
+++ tools/flashlinux.bat
@@ -0,0 +1,25 @@
+REM : Copyright (C) 2022 Avnet
+REM : This BAT script should be called by TTL macro, but not run it directly.
+
+@echo off
+
+REM : Parser command arugments
+set BOARD_IP=%1
+set WORK_DIR=%2
+set IMG_NAME=%3
+
+REM : fastboot commnad is in tools folder
+set PATH=%PATH%;%WORK_DIR%\tools
+
+REM : Fastboot command prefix, add -v to enable verbose mode
+set FB=fastboot -s udp:%BOARD_IP% -v
+
+cd %WORK_DIR%
+
+REM : get u-boot and fastboot version
+%FB% getvar version-bootloader
+%FB% getvar version
+
+REM start flash RAW system image
+echo %FB% flash rawimg %IMG_NAME%
+%FB% flash rawimg %IMG_NAME%
